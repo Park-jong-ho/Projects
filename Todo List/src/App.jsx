180a88f08d23cc254a51e0e8be31d6127bddcceb
jongho <whjk1104@gmail.com>
2024-11-19 13:08:45 +0900

    ✨ Todo List(1/3)
    Todo List(1/3)

--- Todo List/src/App.jsx
+++ Todo List/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, } from "react"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([{
+    id: Number(new Date()), content:'안녕하세요'
+  }])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoInput setTodo={setTodo} />
+      <TodoList todo={todo} setTodo={setTodo}/>
     </>
   )
 }
 
+const TodoInput = ({ setTodo }) => {
+  const inputRef = useRef(null)
+  const addTodo = () => {
+    const newTodo = {
+      id: Number(new Date()),
+      content: inputRef.current.value
+    }
+    setTodo((prev) => [...prev, newTodo])
+  }
+  
+  return (
+    <>
+      <input ref={inputRef}/>
+      <button onClick={addTodo}>추가</button>
+    </>
+  )
+}
+
+const TodoList = ({ todo, setTodo }) => {
+  return (
+    <ul>
+      {todo.map((el) => (
+        <Todo todo={el} setTodo={setTodo}/>
+        ))}
+    </ul>
+  )
+}
+
+const Todo = ({ todo, setTodo }) => {
+  return (
+    <li key={todo.id}>{todo.content}
+    <button onClick={() => {
+      setTodo(prev => prev.filter(el => el.id !== todo.id ))
+    }}>삭제</button>
+    </li>
+  )
+}
 export default App
